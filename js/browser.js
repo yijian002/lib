51d581b16e20245f516edb57687f0613ddce58c1
yijian002 <yijian002@gmail.com>
2017-06-15 11:18:41 +0800

    add weather.iframe, and format code

--- js/browser.js
+++ js/browser.js
@@ -1,17 +1,17 @@
 (function() {
 
-	var browser = {},
-		ua = navigator.userAgent.toLowerCase(),
-		s;
+    var browser = {},
+        ua = navigator.userAgent.toLowerCase(),
+        s;
 
-    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? browser.ie = s[1] :
-    (s = ua.match(/msie ([\d.]+)/)) ? browser.ie = s[1] :
-    (s = ua.match(/edge ([\d.]+)/)) ? browser.ie = s[1] :
-    (s = ua.match(/firefox\/([\d.]+)/)) ? browser.firefox = s[1] :
-    (s = ua.match(/chrome\/([\d.]+)/)) ? browser.chrome = s[1] :
-    (s = ua.match(/opera.([\d.]+)/)) ? browser.opera = s[1] :
-    (s = ua.match(/version\/([\d.]+).*safari/)) ? browser.safari = s[1] : 0;
-    
+    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? browser.ie = s[1]:
+        (s = ua.match(/msie ([\d.]+)/)) ? browser.ie = s[1] :
+        (s = ua.match(/edge ([\d.]+)/)) ? browser.ie = s[1] :
+        (s = ua.match(/firefox\/([\d.]+)/)) ? browser.firefox = s[1] :
+        (s = ua.match(/chrome\/([\d.]+)/)) ? browser.chrome = s[1] :
+        (s = ua.match(/opera.([\d.]+)/)) ? browser.opera = s[1] :
+        (s = ua.match(/version\/([\d.]+).*safari/)) ? browser.safari = s[1] : 0;
+
     if (browser.ie) document.write('IE: ' + browser.ie);
     if (browser.firefox) document.write('Firefox: ' + browser.firefox);
     if (browser.chrome) document.write('Chrome: ' + browser.chrome);
